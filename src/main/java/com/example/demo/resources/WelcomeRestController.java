5638c649054c55f7c790bef609bd2c89f0c9a685
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-13 09:54:33 +0530

    Update WelcomeRestController.java

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 
-@Api(value = "Swagger2WelcomeRestController", description = "This REST Api related to Welcome Message!!!!")
+//@Api(value = "Swagger2WelcomeRestController", description = "This REST Api related to Welcome Message!!!!")
 @RestController
 public class WelcomeRestController {
 
-	@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
+	//@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable("name") String name) {
 		return name+", Welcome to Ashok IT Family..!!";
